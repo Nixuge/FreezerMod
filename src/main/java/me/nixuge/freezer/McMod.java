91604ebcabcf02e89a3949bf90109816af5a79ae
Nixuge <mail@nixuge.me>
2023-07-12 17:33:06 +0200

    Bumped version (1.0.0 -> 1.1.0)

--- src/main/java/me/nixuge/freezer/McMod.java
+++ src/main/java/me/nixuge/freezer/McMod.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class McMod {
     public static final String MOD_ID = "freezer";
     public static final String NAME = "Freezer";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Getter
     @Mod.Instance(value = McMod.MOD_ID)
